f9723247fdf2ae90c658dfd0dc93ed4bf747c43a
Khant Zarni <khantzarni@Leos-Laptop.local>
2024-06-21 00:05:48 +1000

    todo: finalize item/abilities design

--- src/exercises/1-character-card/CharacterCard.jsx
+++ src/exercises/1-character-card/CharacterCard.jsx
@@ -1,10 +1,6 @@
 import { character } from "./characterData";
 // TODO: YOUR CODE HERE
 
-const fetchItems = () => {
-
-}
-
 const CharacterCard = () => {
   return (
     <>
@@ -20,22 +16,46 @@ const CharacterCard = () => {
         <div style={{
           backgroundColor: "beige",
           display: 'flex',
-          alignItems: "center",
-          justifyItems: "center",
+          flexDirection: 'column',
         }}>
-          <p style={{ color: "black", fontWeight: "bold" }}>LV {character.level}</p>
-          <p style={{ color: "black", fontWeight: "bold" }}>HP {character.hp_curr}/{character.hp_max}</p>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: "5em" }}>
+            <p style={{ color: "black", fontWeight: "bold", }}>LV {character.level}</p>
+            <p style={{ color: "black", fontWeight: "bold" }}>HP {character.hp_curr}/{character.hp_max}</p>
+          </div>
+          <div>
+            <img src={character.img} width={"50px"}></img>
+          </div>
+          <div>
+            <p style={{ color: "black" }}>{character.class}</p>
+          </div>
         </div>
+      </div>
+      <div>
         <div style={{
           backgroundColor: "beige",
           display: 'flex',
-          alignItems: "center",
-          justifyItems: "center",
+          flexDirection: 'column',
         }}>
-          <p style={{ color: "black", fontWeight: "bold" }}>LV {character.level}</p>
-          <p style={{ color: "black", fontWeight: "bold" }}>HP {character.hp_curr}/{character.hp_max}</p>
+          <div style={{ display: 'flex', flexDirection: "column" }}>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: "5em" }}>
+              <p style={{ color: "black", fontWeight: "bold", }}>Items</p>
+              <p style={{ color: "black", fontWeight: "bold" }}>Abilities</p>
+            </div>
+            <div style={{ display: "flex", }}>
+              <ul>
+                {character.items.map((item, index) => (
+                  <li key={index} style={{ color: "black", listStyleType: "none" }}>{item.item} x{item.quantity}</li>
+                ))}
+              </ul>
+              <ul>
+                {character.abilities.map((ability, index) => (
+                  <li key={index} style={{ color: "black", listStyleType: "none" }}>{ability}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
+      </div >
     </>
   );
 }
