c093a37c7989b9956fd95638e190c09b7baf4bc9
Khant Zarni <khantzarni@Leos-Laptop.local>
2024-06-20 23:24:54 +1000

    in progress

--- src/exercises/1-character-card/CharacterCard.jsx
+++ src/exercises/1-character-card/CharacterCard.jsx
@@ -1,11 +1,43 @@
 import { character } from "./characterData";
 // TODO: YOUR CODE HERE
+
+const fetchItems = () => {
+
+}
+
 const CharacterCard = () => {
-	return(
-		<>
-			<p>Exercise 1 code here!</p>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <div style={{
+          width: "30em",
+          height: "2em",
+          backgroundColor: "darkblue",
+          border: "2px solid white"
+        }}>
+          <p style={{ color: "white", fontWeight: "bold" }}>{character.name}</p>
+        </div>
+        <div style={{
+          backgroundColor: "beige",
+          display: 'flex',
+          alignItems: "center",
+          justifyItems: "center",
+        }}>
+          <p style={{ color: "black", fontWeight: "bold" }}>LV {character.level}</p>
+          <p style={{ color: "black", fontWeight: "bold" }}>HP {character.hp_curr}/{character.hp_max}</p>
+        </div>
+        <div style={{
+          backgroundColor: "beige",
+          display: 'flex',
+          alignItems: "center",
+          justifyItems: "center",
+        }}>
+          <p style={{ color: "black", fontWeight: "bold" }}>LV {character.level}</p>
+          <p style={{ color: "black", fontWeight: "bold" }}>HP {character.hp_curr}/{character.hp_max}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CharacterCard;
